f1006cb880831c1ca7aec943a6e095429bf1cfbc
JashwantKandhipati <jkandhipati@csu.fullerton.edu>
2026-01-06 13:04:29 -0800

    completed Day 8, learned how create a ceaser cipher, using a function that takes in multiple inputs. Learned how to use index in a for loop to count specific letters

--- Day 8/Positional vs Keyword Arguments/task.py
+++ Day 8/Positional vs Keyword Arguments/task.py
@@ -1,8 +1,17 @@
 # Functions with input
 
-def greet_with_name(name):
-    print(f"Hello {name}")
-    print(f"How do you do {name}?")
+# def greet_with_name(name):
+#     print(f"Hello {name}")
+#     print(f"How do you do {name}?")
+#
+#
+# greet_with_name("Jack Bauer")
 
 
-greet_with_name("Jack Bauer")
+# Function with multiple elements
+
+def greet_with(name, location):
+    print(f"Hello, {name}")
+    print(f"What is it like in {location}")
+
+greet_with(name = "Jay", location =  "IRVINE")
