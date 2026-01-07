d59ec2475c130bd07908d4bc2e2decd9261e8e16
JashwantKandhipati <jkandhipati@csu.fullerton.edu>
2026-01-07 15:44:26 -0800

    Learned what dictionaries are. Nested lists and dictionaries within lists, was able to print out elements from the nested lists/dictionaries. Created Blind Auction Project

--- Day 9/Dictionaries/task.py
+++ Day 9/Dictionaries/task.py
@@ -1,1 +1,29 @@
-programming_dictionary = {"Bug": "An error in a program that prevents the program from running as expected.", "Function": "A piece of code that you can easily call over and over again."}
+programming_dictionary = {
+    "Bug": "An error in a program that prevents the program from running as expected.",
+    "Function": "A piece of code that you can easily call over and over again.",
+}
+
+# how to print a value from a dictionary.
+
+# print(programming_dictionary["Bug"])
+
+# How to add another entry
+programming_dictionary["Loop"] = "The action of doing something over and over again"
+
+# edit an item in the dictionary
+programming_dictionary["Bug"] = "The loop is now changed"
+print(programming_dictionary)
+
+# empty dictionary
+empty_one = {}
+
+# erase all contents of existing dictionary
+    # programming_dictionary = {}
+
+# Looping through a dictionary and print the keys
+for key in programming_dictionary:
+    print(key)
+
+# looping through a dictionary and printing the values
+for key in programming_dictionary:
+    print(programming_dictionary[key])
