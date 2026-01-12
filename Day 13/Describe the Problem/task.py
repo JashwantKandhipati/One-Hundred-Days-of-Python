ce1535ee592cd804fbc17b5494b842332c581a14
JashwantKandhipati <jkandhipati@csu.fullerton.edu>
2026-01-12 09:02:14 -0800

    Finished Day 13, 1 day ago

--- Day 13/Describe the Problem/task.py
+++ Day 13/Describe the Problem/task.py
@@ -1,5 +1,5 @@
 def my_function():
-    for i in range(1, 20):
+    for i in range(1, 21):  # was 20, but changed to 21
         if i == 20:
             print("You got it")
 
@@ -8,5 +8,8 @@ my_function()
 
 # Describe the Problem - Write your answers as comments:
 # 1. What is the for loop doing?
+        # go through the numbers 1-20
 # 2. When is the function meant to print "You got it"?
+        # when i == 20 print "You got it"
 # 3. What are your assumptions about the value of i?
+        # i never actually reaches 20, as 20 is the stop value
--- Day 13/Reproduce the Bug/task.py
+++ Day 13/Reproduce the Bug/task.py
@@ -1,4 +1,4 @@
 from random import randint
 dice_images = ["❶", "❷", "❸", "❹", "❺", "❻"]
-dice_num = randint(1, 6)
+dice_num = randint(0, 5)        # was (1,6) but changed it to (0,5) no out of scope/range error
 print(dice_images[dice_num])
