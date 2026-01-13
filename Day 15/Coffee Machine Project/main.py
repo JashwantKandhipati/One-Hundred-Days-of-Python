0a68c3b4fdbfe46a789e247841e4aeb6166e3df3
JashwantKandhipati <jkandhipati@csu.fullerton.edu>
2026-01-13 11:09:52 -0800

    Finished Day 15, created a pseudo vending machine. Used for loops, while loop, functions with multiple inputs, and dictionaries.

--- Day 15/Coffee Machine Project/main.py
+++ Day 15/Coffee Machine Project/main.py
@@ -24,9 +24,63 @@ MENU = {
     }
 }
 
+profit = 0  # money made from the drinks
 resources = {
     "water": 300,
     "milk": 200,
     "coffee": 100,
 }
 
+def is_resources_sufficient(order_ingredients):
+    """Returns True if there is enough ingredients to make the drink, False if not"""
+    for item in order_ingredients:
+        if order_ingredients[item] > resources[item]:
+            print(f"Sorry there is not enough {item}")
+            return False
+    return True
+
+def process_coins():
+    """Return how much money is received"""
+    print("Please insert coins.")
+    total = int(input("How many quarters: ")) * 0.25
+    total += int(input("How many dimes: ")) * 0.10
+    total += int(input("How many nickels: ")) * 0.05
+    total += int(input("How many pennies: ")) * 0.01
+    return total
+
+def is_transaction_successful(money_received, drink_cost):
+    if money_received >= drink_cost:
+        """Return True if its enough money, False if not"""
+        change = round(money_received - drink_cost, 2)
+        print(f"Here is your change ${change}")
+        global profit
+        profit += drink_cost
+        return True
+    else:
+        print("Sorry that's not enough money, money refunded")
+        return False
+
+def make_coffee(drink_name, order_ingredients):
+    """Subtract the required ingredients from the resources"""
+    for item in order_ingredients:
+        resources[item] -= order_ingredients[item]
+    print(f"Here is your {drink_name} ☕️. Enjoy!")
+
+is_on = True
+
+while is_on:
+    # prompt user for the type of coffee
+    choice = input("What would you like? (espresso/latte/cappuccino): ")
+    if choice == "off":        # secret code word to turn the vending machine off
+        is_on = False
+    elif choice == "report":
+        print(f"Water:  {resources['water']}ml")
+        print(f"Milk:   {resources['milk']}ml")
+        print(f"Coffee: {resources['coffee']}g")
+        print(f"Money: ${profit}")
+    else:
+        drink = MENU[choice]
+        if is_resources_sufficient(drink["ingredients"]):
+            payment = process_coins()
+            if is_transaction_successful(payment, drink["cost"]):
+                make_coffee(choice, drink["ingredients"])
