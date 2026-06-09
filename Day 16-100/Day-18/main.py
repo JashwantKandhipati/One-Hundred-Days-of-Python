6074edc83bb54a34efe0f98de59418ca60a9799c
Jashwant Kandhipati <kandhipati.jashu@gmail.com>
2026-06-09 13:42:05 -0700

    started Day 18

---
+++ Day 16-100/Day-18/main.py
@@ -0,0 +1,31 @@
+from turtle import *
+# from random import *
+
+# from turtle import Turtle
+# from turtle import Screen
+# import turtle as t
+
+
+turtle = Turtle()
+turtle.shape("turtle")
+turtle.color("magenta")
+
+
+for _ in range(4):
+    turtle.forward(100)
+    turtle.right(90)
+
+
+
+
+
+
+
+
+
+
+
+
+
+screen = Screen()
+screen.exitonclick()
