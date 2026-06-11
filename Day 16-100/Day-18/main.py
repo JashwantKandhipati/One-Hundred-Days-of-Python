5f9d9772ecbd51b267d29ea2628c9980e29a47af
jashwantkandhipati <kandhipati.jashu@gmail.com>
2026-06-11 13:07:50 -0700

    drew different shapes continuosly with different colors using random package

--- Day 16-100/Day-18/main.py
+++ Day 16-100/Day-18/main.py
@@ -1,32 +1,41 @@
-from turtle import *
-# from random import *
+# from turtle import *
+import random
 
 # from turtle import Turtle
 # from turtle import Screen
-# import turtle as t
+import turtle as t
+
+tim = t.Turtle()
+
+colors = ["red", "orange", "yellow", "green", "blue", "violet", "purple", "medium slate blue", "spring green", "light salmon"]
+
+# turtle = Turtle()
+# turtle.shape("turtle")
+# turtle.color("blue")
+#
+#
+# for _ in range(15):
+#     turtle.forward(10)
+#     turtle.penup()
+#     turtle.forward(10)
+#     turtle.pendown()
+
+def draw_shape(num_sides):
+    angle = 360 / num_sides
+    for _ in range(num_sides):
+        tim.forward(100)
+        tim.right(angle)
+
+for shape_side_n_ in range(3, 11):
+    tim.color(random.choice(colors))
+    draw_shape(shape_side_n_)
 
 
-turtle = Turtle()
-turtle.shape("turtle")
-turtle.color("blue")
-
-
-for _ in range(15):
-    turtle.forward(10)
-    turtle.penup()
-    turtle.forward(10)
-    turtle.pendown()
 
 
 
 
 
 
-
-
-
-
-
-
-screen = Screen()
+screen = t.Screen()
 screen.exitonclick()
